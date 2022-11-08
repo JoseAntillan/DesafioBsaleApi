95e98bb59972aa4200654a2a7bb99279bc789c05
Jose Antillan <joseantillanm@gmail.com>
2022-11-08 17:23:08 -0300

    agregada la busqueda directamente en la consulta SQL

--- app.py
+++ app.py
@@ -14,12 +14,23 @@ conexion = MySQL(app)
 
 # @cross_origin
 @app.route('/productos', methods=['GET'])
-def listar_cursos():
+def listar_productos():
     try:
-        #nombre = request.args.get("nombre")
+        busqueda = request.args.get("busqueda")
+
 
         cursor = conexion.connection.cursor()
-        sql = "SELECT * FROM product ORDER BY id ASC"
+        # consulta SQL combina la tabla product y category para poder entregar el nombre de la categoria
+        sql =  \
+              " ( SELECT product.id, product.name as producto, product.url_image, product.price, product.discount, product.category, " \
+              " category.id as 'category_id', category.name as 'category_name' " \
+              " FROM product INNER JOIN category ON product.category = category.id ORDER BY product.name ) "
+
+        #si existe el parametro busqueda se usa como filtro en la consulta SQL
+        if busqueda:
+            busqueda = busqueda.strip()
+            sql = "SELECT * FROM " + sql + "AS TablaBusqueda WHERE TablaBusqueda.producto LIKE '%"+busqueda+"%' ORDER BY TablaBusqueda.producto"
+
         cursor.execute(sql)
         datos = cursor.fetchall()
         Productos = []
@@ -30,12 +41,15 @@ def listar_cursos():
                 'url_image': fila[2],
                 'price': fila[3],
                 'discount': fila[4],
-                'category': fila[5]
+                #'Pcategory_id': fila[5],
+                "Ccategory_id": fila[6],
+                "Category_name": fila[7],
             }
             Productos.append(curso)
-        return jsonify({'cursos': Productos, 'mensaje': "Cursos listados.", 'exito': True})
+
+        return jsonify({'Productos': Productos, 'mensaje': "Productos listados.", 'exito': True})
     except Exception as ex:
-        return jsonify({'mensaje': "Error", 'exito': False})
+        return jsonify({'mensaje': "Error"+str(ex), 'exito': False})
 
 ##aqui hacer el metodo para buscar producto en especifico
 
@@ -58,9 +72,9 @@ def leer_curso_bd(categoria):
                 'category': fila[5]
             }
             Productos.append(curso)
-        return jsonify({'cursos': Productos, 'mensaje': "Cursos listados.", 'exito': True})
+        return jsonify({'Productos': Productos, 'mensaje': "Productos listados.", 'exito': True})
     except Exception as ex:
-        return jsonify({'mensaje': "Error: ", 'exito': False})
+        return jsonify({'mensaje': "Error" + str(ex), 'exito': False})
 
 
 
